333f1306837a77fd11a3e265faaf35aae1ab4024
Joe Merhej <joe_merhej33@hotmail.com>
2018-10-21 23:20:37 +0400

    Temporary work on EventManager (started callbacks and logic for fetching events per month)

--- app/src/main/java/com/joemerhej/platform/Event.kt
+++ app/src/main/java/com/joemerhej/platform/Event.kt
@@ -1,6 +1,7 @@
 package com.joemerhej.platform
 
 import android.graphics.Shader
+import android.location.Location
 import com.joemerhej.androidweekview.WeekViewEvent
 import com.joemerhej.androidweekview.WeekViewUtil
 import java.util.*
@@ -8,7 +9,7 @@ import java.util.*
 /**
  * Created by Joe Merhej on 10/17/18.
  */
-class Event(var owner: String, id: String?, title: String?, subtitle: String?, startTime: Calendar, endTime: Calendar, color:Int, allDay: Boolean = false, shader: Shader? = null)
+class Event(var owner: String, var location: Location, id: String?, title: String?, subtitle: String?, startTime: Calendar, endTime: Calendar, color:Int, allDay: Boolean = false, shader: Shader? = null)
     : WeekViewEvent(id, title, subtitle, startTime, endTime, color, allDay, shader)
 {
     init
@@ -16,5 +17,4 @@ class Event(var owner: String, id: String?, title: String?, subtitle: String?, s
         owner = "Joe"
     }
 
-    constructor(owner: String) : this(owner, "bal", "asd", "asd", WeekViewUtil.today(), WeekViewUtil.today(), 0)
 }
--- app/src/main/java/com/joemerhej/platform/EventManager.kt
+++ app/src/main/java/com/joemerhej/platform/EventManager.kt
@@ -1,20 +1,49 @@
 package com.joemerhej.platform
 
+import java.text.DateFormatSymbols
+
 /**
+ * Event manager class will take care of all event storage and actions (serving, fetching...etc)
+ * This class will keep track of active month events in addition to the previous and next month events.
+ *
  * Created by Joe Merhej on 10/17/18.
  */
-class EventManager
+object EventManager
 {
-    var activeMonthEvents: MutableList<Event>? = null
-    var previousMonthEvents: MutableList<Event>? = null
-    var nextMonthEvents: MutableList<Event>? = null
-    var activeMonth: Int = 0
-    var activeMonthYear: Int = 0
-    var previousMonth: Int = 0
-    var previousMonthYear: Int = 0
-    var nextMonth: Int = 0
-    var nextMonthYear: Int = 0
+    private var activeMonthEvents: MutableList<Event>? = mutableListOf()
+    private var previousMonthEvents: MutableList<Event>? = mutableListOf()
+    private var nextMonthEvents: MutableList<Event>? = mutableListOf()
+    private var activeMonth: Int = 0
+    private var activeMonthYear: Int = 0
+    private var previousMonth: Int = 0
+    private var previousMonthYear: Int = 0
+    private var nextMonth: Int = 0
+    private var nextMonthYear: Int = 0
 
+    //TODO CONTINUE THIS INTERFACE (callback function for getMonthEvents)
+    //interface
+
+
+    /**
+     * function that will set the active month and all other EventManager variables based on it.
+     *
+     * @param month the active month to set.
+     * @param year the year of the active month to set.
+     */
+    fun setActiveMonth(month: Int, year: Int)
+    {
+        activeMonth = month
+        activeMonthYear = year
+        //TODO CONTINUE SETTING THIS UP
+    }
+
+    /**
+     * function that will return the events of a specific month.
+     *
+     * @param month the month to return its events.
+     * @param year the year of the month to return its events.
+     * @return mutable list of events.
+     */
     fun getMonthEvents(month: Int, year: Int) : MutableList<Event>?
     {
         // if the month is somehow the same as active month, do nothing
@@ -76,7 +105,7 @@ class EventManager
         }
     }
 
-    // fetch events for single month
+    // fetch events from database
     private fun fetchMonthEvents(month: Int, year: Int) : MutableList<Event>?
     {
         return null

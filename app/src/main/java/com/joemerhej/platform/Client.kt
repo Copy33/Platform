b6f0680c500456a54f42e8f0db8392e4d6827d75
Joe Merhej <joe.merhej@careem.com>
2018-10-24 15:07:17 +0400

    Started designing edit event screen.

---
+++ app/src/main/java/com/joemerhej/platform/Client.kt
@@ -0,0 +1,8 @@
+package com.joemerhej.platform
+
+/**
+ * Created by Joe Merhej on 10/24/18.
+ */
+class Client(var firstName: String?, var lastName: String?, var location: String?)
+{
+}
--- app/src/main/java/com/joemerhej/platform/Event.kt
+++ app/src/main/java/com/joemerhej/platform/Event.kt
@@ -7,12 +7,30 @@ import java.util.*
 /**
  * Created by Joe Merhej on 10/17/18.
  */
-class Event(var owner: String, var location: String, id: String?, title: String?, subtitle: String?, startTime: Calendar, endTime: Calendar, color:Int, allDay: Boolean = false, shader: Shader? = null)
+class Event(
+        id: String?,
+        title: String?,
+        subtitle: String?,
+        startTime: Calendar,
+        endTime: Calendar,
+        color: Int,
+        var owner: String?,
+        var location: String?,
+        var reminder: Calendar?,
+        var client: Client?,
+        allDay: Boolean = false,
+        var eventStatus: Event.EventStatus = EventStatus.UNCONFIRMED,
+        var amountPaid: Double = 0.0,
+        var recurrent: Boolean = false,
+        var notes: String? = null,
+        shader: Shader? = null)
     : WeekViewEvent(id, title, subtitle, startTime, endTime, color, allDay, shader)
 {
-    init
+    enum class EventStatus
     {
-        owner = "Joe"
+        UNCONFIRMED,
+        CONFIRMED,
+        PAID
     }
 
 }

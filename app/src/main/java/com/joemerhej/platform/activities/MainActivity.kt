fa44c005c40c3d5919eab8daddb4511dbb56420d
Joe Merhej <joe_merhej33@hotmail.com>
2018-11-20 12:12:04 +0400

    Moved logic of loading fragments to onDrawerClosed vs. onNavigationItemSelected.

--- app/src/main/java/com/joemerhej/platform/activities/MainActivity.kt
+++ app/src/main/java/com/joemerhej/platform/activities/MainActivity.kt
@@ -5,9 +5,11 @@ package com.joemerhej.platform.activities
  */
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -20,16 +22,22 @@ import com.joemerhej.platform.mainfragments.ScheduleFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.navigation_drawer_header_main.view.*
 
-private const val SAVE_INSTANCE_FRAGMENT_KEY = "activeFragment"
+private const val SAVED_INSTANCE_FRAGMENT_ID_KEY = "activeFragmentId"
+private const val SAVED_INSTANCE_FRAGMENT_TITLE_KEY = "activeFragmentTitle"
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, EditEventDialogFragment.EventListener
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, DrawerLayout.DrawerListener, EditEventDialogFragment.EventListener
 {
     //TODO: This is not needed for now
     override fun onCertainEvent()
     {
     }
 
+    // fragment to load and its id
+    private lateinit var fragment: Fragment
+    private lateinit var fragmentTitle: String
     private var fragmentId = 0
+    private var clickedNavItem = false
+
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -39,12 +47,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // initialize the toolbar
         setSupportActionBar(edit_event_toolbar)
 
-        // initialize toolbar toggle and navigation drawer
+        // initialize the toolbar toggle
         val toggle = ActionBarDrawerToggle(this, drawer_layout, edit_event_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
+        // set up navigation drawer and drawer layout listeners
         navigation_drawer.setNavigationItemSelectedListener(this)
+        drawer_layout.addDrawerListener(this)
 
         // load logo in navigation drawer header image
         val navigationDrawerHeader = navigation_drawer.getHeaderView(0)
@@ -53,16 +63,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 .apply(RequestOptions().centerCrop().circleCrop())
                 .into(navigationDrawerHeader.nav_header_imageview)
 
-        // if saved instance contains a fragment id (screen rotation) then load it
+        // if saved instance contains a fragment id (screen rotation) then fetch it and update UI elements (title, nav drawer checked item)
         savedInstanceState?.let {
-            val fragmentIdInstance = savedInstanceState.getInt(SAVE_INSTANCE_FRAGMENT_KEY)
-            navigation_drawer.menu.findItem(fragmentIdInstance).isChecked = true
-            fragmentId = fragmentIdInstance
+            fragmentId = savedInstanceState.getInt(SAVED_INSTANCE_FRAGMENT_ID_KEY)
+            navigation_drawer.menu.findItem(fragmentId).isChecked = true
+            fragmentTitle = savedInstanceState.getString(SAVED_INSTANCE_FRAGMENT_TITLE_KEY, resources.getString(R.string.nav_item_schedule))
+            supportActionBar?.title = fragmentTitle
         } ?: run {
             //  else load the default (schedule) fragment
             supportFragmentManager.beginTransaction().replace(R.id.frameLayoutContent, ScheduleFragment.newInstance()).commit()
             fragmentId = R.id.nav_item_schedule
-            supportActionBar?.title = resources.getString(R.string.nav_item_schedule)
+            navigation_drawer.menu.findItem(fragmentId).isChecked = true
+            fragmentTitle = resources.getString(R.string.nav_item_schedule)
+            supportActionBar?.title = fragmentTitle
         }
     }
 
@@ -70,8 +83,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     {
         super.onSaveInstanceState(outState)
 
-        // save fragment id on save instance state (on screen rotation)
-        outState?.putInt(SAVE_INSTANCE_FRAGMENT_KEY, fragmentId)
+        // save fragment title and id on save instance state (on screen rotation)
+        outState?.putString(SAVED_INSTANCE_FRAGMENT_TITLE_KEY, fragmentTitle)
+        outState?.putInt(SAVED_INSTANCE_FRAGMENT_ID_KEY, fragmentId)
     }
 
     override fun onBackPressed()
@@ -84,30 +98,38 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     /**
-     * Navigation drawer item selected listener to handle the menu items clicks.
+     * Navigation drawer item selected listener to handle the drawer items clicks.
+     * This call will only create the fragment instance based on drawer item click but will not replace it.
+     * The fragment will be replaced in the DrawerLayoutListener.onDrawerClosed to avoid the lagging
+     * and stuttering that happens if we try to close the drawer and load the fragment at the same time.
      *
      * @param item menu item clicked.
      */
     override fun onNavigationItemSelected(item: MenuItem): Boolean
     {
-        lateinit var fragment: Fragment
-        lateinit var fragmentClass: Class<*>
+        if(item.itemId == fragmentId)
+        {
+            drawer_layout.closeDrawer(GravityCompat.START)
+            return true
+        }
 
         fragmentId = item.itemId
+        fragmentTitle = item.title.toString()
+        clickedNavItem = true
 
         when(fragmentId)
         {
             R.id.nav_item_schedule ->
             {
-                fragmentClass = ScheduleFragment::class.java
+                fragment = ScheduleFragment.newInstance()
             }
             R.id.nav_item_clients ->
             {
-                fragmentClass = ClientsFragment::class.java
+                fragment = ClientsFragment.newInstance()
             }
             R.id.nav_item_owners ->
             {
-                fragmentClass = OwnersFragment::class.java
+                fragment = OwnersFragment.newInstance()
             }
             R.id.nav_item_share ->
             {
@@ -118,20 +140,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
             }
             else ->
-                fragmentClass = ScheduleFragment::class.java
+                fragment = ScheduleFragment.newInstance()
         }
-
-        try
-        {
-            fragment = fragmentClass.newInstance() as Fragment
-        }
-        catch(e: Exception)
-        {
-            e.printStackTrace()
-        }
-
-        // Insert the fragment by replacing the existing fragment
-        supportFragmentManager.beginTransaction().replace(R.id.frameLayoutContent, fragment).commit()
 
         // change the toolbar title
         supportActionBar?.title = item.title
@@ -142,6 +152,37 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // close the navigation drawer
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onDrawerStateChanged(newState: Int)
+    {
+    }
+
+    override fun onDrawerSlide(drawerView: View, slideOffset: Float)
+    {
+    }
+
+    /**
+     * Load the fragment instance created earlier (in the drawer navigation on item click listener)
+     * into the activity's frame layout. This is done here to avoid lagging the drawer when clicking
+     * an item inside it since it won't close it and load the fragment at the same time.
+     *
+     * @param drawerView view of the drawer (not used).
+     */
+    override fun onDrawerClosed(drawerView: View)
+    {
+        if(clickedNavItem)
+        {
+            clickedNavItem = false
+
+            // Insert the fragment by replacing the existing fragment
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayoutContent, fragment).commit()
+        }
+    }
+
+    override fun onDrawerOpened(drawerView: View)
+    {
     }
 }
 
